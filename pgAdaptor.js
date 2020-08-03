9f6a5f41a862b597c19ae940361c0d1cc0d3d46e
Deepak Ponnuswamy <deepak.ponnuswamy@gmail.com>
2020-08-03 16:28:33 -0500

    heroku additions

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -3,14 +3,14 @@ const pgPromise = require('pg-promise');
 
 const pgp = pgPromise({}); // Empty object means no additional config required
 
-const config = {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
-};
+// const config = {
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     database: process.env.POSTGRES_DB,
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD
+// };
 
-const db = pgp(config);
+const db = pgp(process.env.DATABASE_URL);
 
 exports.db = db;
