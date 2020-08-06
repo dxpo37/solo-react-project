83d3f18bf6936e4f19050e695f7fb323c4232b6b
Deepak Ponnuswamy <deepak.ponnuswamy@gmail.com>
2020-08-06 15:57:42 -0500

    heroku deploy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const app = require("express")()
+const express = require("express")
 const { graphqlHTTP } = require('express-graphql')
 const cors = require('cors')
 const morgan = require("morgan")
@@ -8,14 +9,16 @@ const morgan = require("morgan")
 const {requireAuth} = require("./utils/auth")
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
   app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
+let url
+process.env.NODE_ENV==="development" ? url="http://localhost:3000" : url="https://solo-react-project.herokuapp.com/"
 
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:url,
   credentials:true
 }))
 app.use(morgan("dev"));
