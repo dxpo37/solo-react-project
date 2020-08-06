51435f9a2030c93ee0a5e9699089e8478d20ef85
Deepak Ponnuswamy <deepak.ponnuswamy@gmail.com>
2020-08-06 15:30:10 -0500

    heroku deploy

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const morgan = require("morgan")
 
 const {requireAuth} = require("./utils/auth")
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(cors({
   origin:"http://localhost:3000",
   credentials:true
