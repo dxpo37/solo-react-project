214a5687333d480d97939e9131026f2d881c43d2
Deepak Ponnuswamy <deepak.ponnuswamy@gmail.com>
2020-08-08 22:31:37 -0500

    add aws s3 file upload. not returning url but file is uploading

--- client/src/Components/UploadPost.jsx
+++ client/src/Components/UploadPost.jsx
@@ -8,19 +8,20 @@ import {envVars} from '../Config/index'
 export default function UploadPost() {
 
   const uploadHandler = async (e) => {
+    let awsConfig
     if(!envVars.awsConfig.secretAccessKey){
       const response = await fetch("/aws")
-      const awsConfig = await response.json()
+      awsConfig = await response.json()
       console.log("No Token---> but....", awsConfig )
       console.log("No Token---> but....KEYYYY", awsConfig.secretAccessKey )
     }
-    else { const awsConfig = envVars.awsConfig}
-    console.log("deepak--->" , aws)
+    else { awsConfig = envVars.awsConfig}
+  
     const file = document.querySelector("[name=fileUpload]").files[0]
     let postUrl
     const ReactS3Client = new S3(awsConfig); 
-    try{ postUrl = await ReactS3Client.uploadFile(file, file.name)}
-    catch{ console.log("ZZZZZZZZZZZZZZ")}
+    try { postUrl = await ReactS3Client.uploadFile(file, file.name)}
+    catch{ console.log("ZZZZZZZZZZZZZZ", postUrl)}
   }
 
   return (
